e821f82bd2d53962cdf3074b1af692aed0d0d0c1
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2024-05-27 12:23:19 +0500

    style learn page

--- src/routes/(public)/learn/[id]/+page.svelte
+++ src/routes/(public)/learn/[id]/+page.svelte
@@ -6,21 +6,30 @@
 	// console.log('/learn/id :: data => ', data.post);
 </script>
 
-<article>
-	{#if post}
-		<h2
-			class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+<main class="bg-white pb-16 pt-8 antialiased dark:bg-gray-900 lg:pb-24 lg:pt-16">
+	<div class="mx-auto flex max-w-screen-xl justify-between px-4">
+		<article
+			class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert mx-auto w-full max-w-2xl"
 		>
-			{post.title}
-		</h2>
-		{#if post.coverImage}
-			<img src={post.coverImage} alt="cover_image" />
-		{/if}
-		<p class="italic">
-			by {post.userEmail} - {post.createdAt.toLocaleString('en-GB', {
-				timeZone: 'UTC'
-			})}
-		</p>
-		<p class="mt-2 text-gray-500">{@html post?.content}</p>
-	{/if}
-</article>
+			{#if post}
+				<h2
+					class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+				>
+					{post.title}
+				</h2>
+
+				<p class="italic">
+					by {post.userEmail} - {post.createdAt.toLocaleString('en-GB', {
+						timeZone: 'UTC'
+					})}
+				</p>
+				{#if post.coverImage}
+					<div class="flex justify-center">
+						<img src={post.coverImage} alt="cover_image" class="h-auto max-w-full rounded-lg" />
+					</div>
+				{/if}
+				<p class="mt-2 text-gray-500">{@html post?.content}</p>
+			{/if}
+		</article>
+	</div>
+</main>
