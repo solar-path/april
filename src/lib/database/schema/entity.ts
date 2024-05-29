4af68f28e7b497e1b27fd6df60a416ea00a58887
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2024-05-29 18:06:50 +0500

    group structure tables

--- src/lib/database/schema/entity.ts
+++ src/lib/database/schema/entity.ts
@@ -15,6 +15,64 @@ export const entityTypes = [
 
 export const entityTypeEnum = pgEnum('entity_type', entityTypes);
 
+export const workspaceTable = pgTable('workspaces', {
+	id: varchar('id', {
+		length: 50
+	}).primaryKey(),
+	title: varchar('title', { length: 250 }).notNull().unique(),
+	authorId: varchar('authorId', { length: 50 }).references(() => userTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
+export const regionTable = pgTable('regions', {
+	id: varchar('id', {
+		length: 50
+	}).primaryKey(),
+	title: varchar('title', { length: 250 }).notNull(),
+	workspaceId: varchar('workspaceId', { length: 50 }).references(() => workspaceTable.id),
+	authorId: varchar('authorId', { length: 50 }).references(() => userTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
+export const companyTypeEnum = pgEnum('company_type', ['company', 'counterparty']);
+
+export const companyTable = pgTable('companies', {
+	id: varchar('id', {
+		length: 50
+	}).primaryKey(),
+	title: varchar('title', { length: 250 }).notNull(),
+	type: companyTypeEnum('company_type').notNull(),
+	regionId: varchar('regionId', { length: 50 }).references(() => regionTable.id),
+	workspaceId: varchar('workspaceId', { length: 50 }).references(() => workspaceTable.id),
+	authorId: varchar('authorId', { length: 50 }).references(() => userTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
+export const departmentTable = pgTable('departments', {
+	id: varchar('id', {
+		length: 50
+	}).primaryKey(),
+	title: varchar('title', { length: 250 }).notNull(),
+	companyId: varchar('companyId', { length: 50 }).references(() => companyTable.id),
+	authorId: varchar('authorId', { length: 50 }).references(() => userTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
+export const positionTable = pgTable('positions', {
+	id: varchar('id', {
+		length: 50
+	}).primaryKey(),
+	title: varchar('title', { length: 250 }).notNull(),
+	departmentId: varchar('departmentId', { length: 50 }).references(() => departmentTable.id),
+	authorId: varchar('authorId', { length: 50 }).references(() => userTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
 export const entityTable: any = pgTable('entities', {
 	id: varchar('id', {
 		length: 50
