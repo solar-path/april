a3069388de118deb5accf4b4dddc2117ddf04515
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2024-06-14 12:28:12 +0500

    position CRUD: done

--- src/routes/dashboard/entity/+page.server.ts
+++ src/routes/dashboard/entity/+page.server.ts
@@ -15,7 +15,7 @@ import { deleteWorkspaceSchema, workspaceSchema } from './Validation/workspace.s
 import { deleteRegionSchema, regionSchema } from './Validation/region.schema';
 import { companySchema, deleteCompanySchema } from './Validation/company.schema';
 import { deleteDepartmentSchema, departmentSchema } from './Validation/department.schema';
-import { positionSchema } from './Validation/position.schema';
+import { deletePositionSchema, positionSchema } from './Validation/position.schema';
 import { addressTable } from '$lib/database/schema/address';
 import { eq } from 'drizzle-orm';
 import { contactTable } from '$lib/database/schema/contact';
@@ -402,24 +402,71 @@ export const actions: Actions = {
 	},
 	// POSITION CRUD
 	createPosition: async (event) => {
+		console.log('create position endpoint reached');
 		const form = await superValidate(await event.request.formData(), zod(positionSchema));
 		if (!form.valid) {
+			console.log('form is not valid => ', form);
 			return fail(400, { form });
 		}
+
+		await db.insert(positionTable).values({
+			id: crypto.randomUUID(),
+			title: form.data.title,
+			description: form.data.description,
+			departmentId: form.data.departmentId,
+			companyId: form.data.companyId,
+			author: event.locals.user?.id as string
+		});
 		return { form };
 	},
 	updatePosition: async (event) => {
+		console.log('update position endpoint reached');
 		const form = await superValidate(await event.request.formData(), zod(positionSchema));
 		if (!form.valid) {
+			console.log('form is not valid => ', form);
 			return fail(400, { form });
 		}
+		const record = await db
+			.select()
+			.from(positionTable)
+			.where(eq(positionTable.id, form.data.id as string));
+
+		await db
+			.update(positionTable)
+			.set({
+				title: record[0].title !== form.data.title ? form.data.title : record[0].title,
+				description:
+					record[0].description !== form.data.description
+						? form.data.description
+						: record[0].description,
+				departmentId:
+					record[0].departmentId !== form.data.departmentId
+						? form.data.departmentId
+						: record[0].departmentId,
+				companyId:
+					record[0].companyId !== form.data.companyId ? form.data.companyId : record[0].companyId
+			})
+			.where(eq(positionTable.id, form.data.id as string));
 		return { form };
 	},
 	deletePosition: async (event) => {
-		const form = await superValidate(await event.request.formData(), zod(positionSchema));
+		console.log('delete position endpoint reached');
+		const form = await superValidate(await event.request.formData(), zod(deletePositionSchema));
 		if (!form.valid) {
+			console.log('form is not valid => ', form);
 			return fail(400, { form });
 		}
+
+		const record = await db
+			.select()
+			.from(positionTable)
+			.where(eq(positionTable.id, form.data.id as string));
+
+		if (record.length === 0) {
+			return fail(400, { form, error: 'Position not found' });
+		}
+
+		await db.delete(positionTable).where(eq(positionTable.id, form.data.id as string));
 		return { form };
 	}
 	// 	createUnit: async (event) => {
--- src/routes/dashboard/entity/Forms/Position.form.svelte
+++ src/routes/dashboard/entity/Forms/Position.form.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import DisplayFormErrors from '$lib/components/DisplayFormErrors.svelte';
 	import { hideDrawer } from '$lib/components/Drawer/drawer.utlities';
-	import { Button, Input, Label } from 'flowbite-svelte';
+	import SelectWithSearchTree from '$lib/components/SelectWithSearch/SelectWithSearchTree.svelte';
+	import { formStore } from '$lib/components/form/formStore';
+	import { Button, Input, Label, Textarea } from 'flowbite-svelte';
+	import { onDestroy } from 'svelte';
 	import SuperDebug, { superForm, type FormResult } from 'sveltekit-superforms';
 
 	interface PositionData {
 		item?: {
 			id?: string;
 			title: string;
+			description: string;
 			departmentId: string;
 			department: string;
 			companyId: string;
@@ -28,6 +32,7 @@
 					...data.positionForm.data,
 					id: data.item.id,
 					title: data.item.title,
+					description: data.item.description,
 					departmentId: data.item.departmentId,
 					department: data.item.department,
 					companyId: data.item.companyId,
@@ -43,23 +48,86 @@
 			}
 		}
 	);
+
+	$: filteredDepartmentList = data.departmentList.filter(
+		(department) => department.companyId === $form.companyId
+	);
+
+	const unsubscribe = formStore.subscribe((value) => {
+		if (value.companyId) {
+			$form.companyId = value.companyId.fieldId;
+			$form.company = value.companyId.fieldName;
+		}
+		if (value.departmentId) {
+			$form.departmentId = value.departmentId.fieldId;
+			$form.department = value.departmentId.fieldName;
+		}
+	});
+
+	onDestroy(() => {
+		unsubscribe();
+	});
 </script>
 
 <form
 	use:enhance
 	novalidate
 	method="POST"
-	action={data.item ?? data.item !== null
-		? '/dashboard/entity/updatePosition'
-		: '/dashboard/entity/createPosition'}
+	action={data.item && data.item !== null
+		? '/dashboard/entity?/updatePosition'
+		: '/dashboard/entity?/createPosition'}
 	class="flex flex-col space-y-2"
 >
 	<input type="hidden" name="id" bind:value={$form.id} />
 
 	<div class="w-full">
-		<Label for="title">Position title</Label>
+		<Label for="title">Position</Label>
 		<Input id="title" type="text" name="title" bind:value={$form.title} {...$constraints.title} />
 		<DisplayFormErrors errors={$errors.title} />
+	</div>
+
+	<div class="w-full">
+		<Label for="description">Description</Label>
+		<Textarea
+			id="description"
+			type="text"
+			name="description"
+			bind:value={$form.description}
+			{...$constraints.description}
+		/>
+		<DisplayFormErrors errors={$errors.description} />
+	</div>
+
+	<div class="w-full">
+		<SelectWithSearchTree
+			label="Company"
+			list={data.companyList}
+			tree={data.companyList}
+			form={$form}
+			errors={$errors}
+			constraints={$constraints}
+			modalID="company"
+			modalState={false}
+			fieldId="companyId"
+			fieldName="company"
+		/>
+		<DisplayFormErrors errors={$errors.companyId} />
+	</div>
+
+	<div class="w-full">
+		<SelectWithSearchTree
+			label="Department"
+			list={filteredDepartmentList}
+			tree={filteredDepartmentList}
+			form={$form}
+			errors={$errors}
+			constraints={$constraints}
+			modalID="department"
+			modalState={false}
+			fieldId="departmentId"
+			fieldName="department"
+		/>
+		<DisplayFormErrors errors={$errors.departmentId} />
 	</div>
 
 	<Button type="submit" class="w-full">Add</Button>
