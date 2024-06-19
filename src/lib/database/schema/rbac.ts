a6d4d33ed59cac6ae9a8c1967a91dc0917517b06
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2024-06-19 17:31:30 +0500

    start rbac

---
+++ src/lib/database/schema/rbac.ts
@@ -0,0 +1,39 @@
+import { pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { userTable } from './users';
+
+export const roleTable = pgTable('rbac_role', {
+	id: varchar('id', { length: 50 }).primaryKey(),
+	title: varchar('title', { length: 50 }).notNull().unique(),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
+export const userRoleTable = pgTable('rbac_user_role', {
+	id: varchar('id', { length: 50 }).primaryKey(),
+	userId: varchar('userId', { length: 50 }).references(() => userTable.id),
+	roleId: varchar('roleId', { length: 50 }).references(() => roleTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
+export const permissionTable = pgTable('rbac_permission', {
+	id: varchar('id', { length: 50 }).primaryKey(),
+	title: varchar('title', { length: 50 }).notNull().unique()
+});
+
+export const rolePermissionTable = pgTable('rbac_role_permission', {
+	id: varchar('id', { length: 50 }).primaryKey(),
+	roleId: varchar('roleId', { length: 50 }).references(() => roleTable.id),
+	permissionId: varchar('permissionId', { length: 50 }).references(() => permissionTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
+
+// Optional: User-specific permissions
+export const userPermissionTable = pgTable('rbac_user_permission', {
+	id: varchar('id', { length: 50 }).primaryKey(),
+	userId: varchar('userId', { length: 50 }).references(() => userTable.id),
+	permissionId: varchar('permissionId', { length: 50 }).references(() => permissionTable.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull().defaultNow()
+});
