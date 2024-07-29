b70f7eea5949d03dd208b8ba8ae7508b046a6fba
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2024-07-29 13:47:44 +0500

    resolve error at src/routes/[slug]/entity/company/+page.server.js (22:61): Expected ',', got 'as'

--- src/routes/[slug]/entity/company/+page.server.js
+++ src/routes/[slug]/entity/company/+page.server.js
@@ -1,71 +1,68 @@
-import { fail, superValidate, withFiles } from "sveltekit-superforms";
-import { companySchema, deleteCompanySchema } from "./company.schema";
-import { zod } from "sveltekit-superforms/adapters";
-import { db } from "$lib/database/db";
-import { companyTable } from "$lib/database/schema/entity";
-import { eq } from "drizzle-orm";
-import { fileProcessor } from "$lib/helpers/fileProcessor";
+import { fail, superValidate, withFiles } from 'sveltekit-superforms';
+import { companySchema, deleteCompanySchema } from './company.schema';
+import { zod } from 'sveltekit-superforms/adapters';
+import { db } from '$lib/database/db';
+import { companyTable } from '$lib/database/schema/entity';
+import { eq } from 'drizzle-orm';
+import { fileProcessor } from '$lib/helpers/fileProcessor';
 
 export const actions = {
-    // COMPANY CRUD
-createCompany: async (event) => {
-    const form = await superValidate(await event.request.formData(), zod(companySchema));
+	// COMPANY CRUD
+	createCompany: async (event) => {
+		const form = await superValidate(await event.request.formData(), zod(companySchema));
 
-    if (!form.valid) {
-        console.log('invalid form => ', form);
-        if (!form.valid) return fail(400, withFiles({ form }));
-    }
+		if (!form.valid) {
+			console.log('invalid form => ', form);
+			if (!form.valid) return fail(400, withFiles({ form }));
+		}
 
-    const checkCompany = await db
-        .select()
-        .from(companyTable)
-        .where(eq(companyTable.title, form.data.title.trim() as string));
+		const checkCompany = await db
+			.select()
+			.from(companyTable)
+			.where(eq(companyTable.title, form.data.title.trim()));
 
-    if (checkCompany.length > 0) {
-        return fail(400, { form, error: 'Company already exists' });
-    }
+		if (checkCompany.length > 0) {
+			return fail(400, { form, error: 'Company already exists' });
+		}
 
-    await db.insert(companyTable).values({
-        id: crypto.randomUUID(),
-        title: form.data.title.trim(),
-        description: form.data.description?.trim(),
-        logo: form.data.logo instanceof File ? await fileProcessor(form.data.logo, 'logo') : '',
-        type: 'company', // Ensure this is a valid value
-        regionId: form.data.regionId,
-        workspaceId: form.data.workspaceId,
-        industryId: form.data.industryId,
-        BIN: form.data.BIN.trim().toUpperCase(),
-        author: event.locals.user?.id as string
-    });
+		await db.insert(companyTable).values({
+			id: crypto.randomUUID(),
+			title: form.data.title.trim(),
+			description: form.data.description?.trim() || '',
+			logo: form.data.logo instanceof File ? await fileProcessor(form.data.logo, 'logo') : '',
+			type: 'company',
+			regionId: form.data.regionId || '',
+			workspaceId: form.data.workspaceId || '',
+			industryId: form.data.industryId || '',
+			BIN: form.data.BIN.trim().toUpperCase(),
+			author: event.locals.user?.id || ''
+		});
 
-    return withFiles({ form });
-},
-updateCompany: async (event) => {
-    console.log('update company endpoint reached');
-    const form = await superValidate(await event.request.formData(), zod(companySchema));
-    if (!form.valid) {
-        console.log('form is not valid => ', form);
-        return fail(400, { form });
-    }
-    return { form };
-},
-deleteCompany: async (event) => {
-    console.log('delete company endpoint reached');
-    const form = await superValidate(await event.request.formData(), zod(deleteCompanySchema));
-    if (!form.valid) {
-        console.log('form is not valid => ', form);
-        return fail(400, { form });
-    }
-    const record = await db
-        .select()
-        .from(companyTable)
-        .where(eq(companyTable.id, form.data.id as string));
+		return withFiles({ form });
+	},
+	updateCompany: async (event) => {
+		console.log('update company endpoint reached');
+		const form = await superValidate(await event.request.formData(), zod(companySchema));
+		if (!form.valid) {
+			console.log('form is not valid => ', form);
+			return fail(400, { form });
+		}
+		return { form };
+	},
+	deleteCompany: async (event) => {
+		console.log('delete company endpoint reached');
+		const form = await superValidate(await event.request.formData(), zod(deleteCompanySchema));
+		if (!form.valid) {
+			console.log('form is not valid => ', form);
+			return fail(400, { form });
+		}
+		const record = await db.select().from(companyTable).where(eq(companyTable.id, form.data.id));
 
-    if (record.length === 0) {
-        return fail(400, { form, error: 'Company not found' });
-    }
+		if (record.length === 0) {
+			return fail(400, { form, error: 'Company not found' });
+		}
 
-    await db.delete(companyTable).where(eq(companyTable.id, form.data.id as string));
-    return { form };
-},
-}
+		await db.delete(companyTable).where(eq(companyTable.id, form.data.id));
+		return { form };
+	}
+};
