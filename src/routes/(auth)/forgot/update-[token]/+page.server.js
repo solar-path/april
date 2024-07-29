31cb6a3dd8eef5dabb2134ed54ec711aca430f7f
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2024-07-29 13:40:05 +0500

    resolve issue at (auth)/forgot/update-[toke]/+page.server.js

--- src/routes/(auth)/forgot/update-[token]/+page.server.js
+++ src/routes/(auth)/forgot/update-[token]/+page.server.js
@@ -7,7 +7,6 @@ import { db } from '$lib/database/db.js';
 import { userTable } from '$lib/database/schema/users';
 import { eq } from 'drizzle-orm/mysql-core/expressions';
 
-
 export const load = async () => {
 	const changePasswordForm = await superValidate(zod(changePasswordSchema));
 	return { changePasswordForm };
@@ -15,7 +14,7 @@ export const load = async () => {
 
 export const actions = {
 	default: async ({ params, request }) => {
-		const token = params.token as string;
+		const token = params.token;
 		console.log('forgot/update-[token]/+page.server.ts :: token => ', token);
 		const form = await superValidate(await request.formData(), zod(changePasswordSchema));
 
@@ -24,10 +23,7 @@ export const actions = {
 		}
 
 		// find user based on token
-		const existingUser = await db
-			.select()
-			.from(userTable)
-			.where(eq(userTable.token, token));
+		const existingUser = await db.select().from(userTable).where(eq(userTable.token, token));
 
 		if (!existingUser[0]) {
 			return setError(form, 'password', 'User not registered');
@@ -37,7 +33,10 @@ export const actions = {
 			return setError(form, 'password', 'Account was not verified');
 		}
 
-		await db.update(userTable).set({ password: await new Argon2id().hash(form.data.password) }).where(eq(userTable.token, token));
+		await db
+			.update(userTable)
+			.set({ password: await new Argon2id().hash(form.data.password) })
+			.where(eq(userTable.token, token));
 
 		redirect(302, '/login');
 	}
