2b45a016c4bea8e07542d3fba0c3241ee8631895
Assanali Tungat <ali@Assanalis-MacBook-Air.local>
2024-08-01 16:24:04 +0500

    remove regionTable from schema/entity.ts

--- src/lib/database/schema/entity.ts
+++ src/lib/database/schema/entity.ts
@@ -20,22 +20,6 @@ export const workspaceTable = pgTable('structure_workspaces', {
 	updatedAt: timestamp('updatedAt').notNull().defaultNow()
 });
 
-export const regionTable = pgTable('structure_regions', {
-	id: varchar('id', {
-		length: 50
-	}).primaryKey(),
-	title: varchar('title', { length: 250 }).notNull(),
-	description: text('description'),
-	workspaceId: varchar('workspaceId', { length: 50 }).references(() => workspaceTable.id),
-	author: varchar('author', { length: 50 })
-		.notNull()
-		.references(() => userTable.id),
-	createdAt: timestamp('createdAt').notNull().defaultNow(),
-	updatedAt: timestamp('updatedAt').notNull().defaultNow()
-});
-
-export const companyTypeEnum = pgEnum('company_type', ['company', 'counterparty']);
-
 export const companyTable = pgTable('structure_companies', {
 	id: varchar('id', {
 		length: 50
@@ -44,10 +28,6 @@ export const companyTable = pgTable('structure_companies', {
 	description: text('description'),
 	company: varchar('company', { length: 250 }).notNull().unique(),
 	logo: varchar('logo', { length: 250 }),
-	type: companyTypeEnum('company_type').notNull(),
-	regionId: varchar('regionId', { length: 50 })
-		.notNull()
-		.references(() => regionTable.id),
 	workspaceId: varchar('workspaceId', { length: 50 })
 		.notNull()
 		.references(() => workspaceTable.id),
